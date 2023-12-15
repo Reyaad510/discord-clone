a26419a0c86ea6463ec1f96163208415c3eda7b4
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2023-12-14 23:27:27 -0500

    Changed font to opensans

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,9 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
  
+// cn function helps allow create dynamic classes in tailwind
+// conflicting classes can occur and cn util solves that
+// Ex <Button className={cn("bg-indigo-50", state && "bg-red-500")}>Click Me!</Button>
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
